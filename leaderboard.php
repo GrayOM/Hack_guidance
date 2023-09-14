7fe2f19c30f6de9e4d79ab13f774ce2161ecd506
GrayOM <tmdals720@naver.com>
2023-09-14 11:19:48 +0900

    v2

--- leaderboard.php
+++ leaderboard.php
@@ -1,14 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ko">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>마우스 호버로 이미지 확대</title>
-    <link rel="stylesheet" href="styles.css">
+    <title>워게임 사이트</title>
+    <link rel="stylesheet" href="style.css"> <!-- 스타일 시트 파일 -->
 </head>
 <body>
-    <div class="image-container">
-        <img src="testimage.png" alt="">
-    </div>
+    <header>
+        <nav>
+            <ul class="font_menu">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="about.php">Challenge</a></li>
+                <li><a href="leaderboard.php">Ranking(미완성)</a></li>
+                <li><a href="contact.php">Contact(미완성)</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <aside class="sidebar">
+            <ul class="sidebar-menu">
+                <li><a href="#">메뉴1</a></li>
+                <li><a href="#">메뉴2</a></li>
+                <li><a href="#">메뉴3</a></li>
+                <li><a href="#">메뉴4</a></li>
+                <li><a href="#">메뉴5</a></li>
+            </ul>
+        </aside>
+        <section>
+            <!-- 본문 내용 추가 -->
+        </section>
+    </main>
+    <footer>
+        <p>&copy; Hack guidance</p>
+    </footer>
 </body>
 </html>
